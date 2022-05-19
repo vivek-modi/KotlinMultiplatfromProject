155f11128edd02855cb1907a64daf8de91a0b794
Vivek Modi <vivekmodi456@gmail.com>
2022-05-19 10:17:12 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    kotlin("multiplatform") version "1.6.21"
+    id("com.android.library")
+    id("maven-publish")
+}
+
+group = "me.vmodi"
+version = "1.0.0"
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+kotlin {
+    android()
+    android {
+        publishLibraryVariants("release", "debug")
+    }
+    sourceSets {
+        val commonMain by getting
+        val androidMain by getting
+    }
+}
+
+android {
+    compileSdk = 21
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 31
+    }
+    @Suppress("UnstableApiUsage") compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
---
+++ src/commonMain/kotlin/Platfrom.kt
@@ -0,0 +1,1 @@
+fun getMessage() = "Hello World"
