ae8818d0da2b4b716b2a8c1fde087603202372ac
Vivek Modi <vivekmodi456@gmail.com>
2022-05-22 11:39:03 +0100

    added cocoapods

---
+++ MyCocoaPod.podspec
@@ -0,0 +1,39 @@
+Pod::Spec.new do |spec|
+    spec.name                     = 'MyCocoaPod'
+    spec.version                  = '0.0.1'
+    spec.homepage                 = 'Link to a Kotlin/Native module homepage'
+    spec.source                   = { :http=> ''}
+    spec.authors                  = ''
+    spec.license                  = ''
+    spec.summary                  = 'Some description for a Kotlin/Native module'
+    spec.vendored_frameworks      = 'build/cocoapods/framework/MyFramework.framework'
+    spec.libraries                = 'c++'
+                
+                
+                
+    spec.pod_target_xcconfig = {
+        'KOTLIN_PROJECT_PATH' => ':',
+        'PRODUCT_MODULE_NAME' => 'MyFramework',
+    }
+                
+    spec.script_phases = [
+        {
+            :name => 'Build MyCocoaPod',
+            :execution_position => :before_compile,
+            :shell_path => '/bin/sh',
+            :script => <<-SCRIPT
+                if [ "YES" = "$COCOAPODS_SKIP_KOTLIN_BUILD" ]; then
+                  echo "Skipping Gradle build task invocation due to COCOAPODS_SKIP_KOTLIN_BUILD environment variable set to \"YES\""
+                  exit 0
+                fi
+                set -ev
+                REPO_ROOT="$PODS_TARGET_SRCROOT"
+                "$REPO_ROOT/../../../../private/var/folders/bh/nx_31y193190m5vz8sd9y6jm0000gq/T/wrap2loc/gradlew" -p "$REPO_ROOT" $KOTLIN_PROJECT_PATH:syncFramework \
+                    -Pkotlin.native.cocoapods.platform=$PLATFORM_NAME \
+                    -Pkotlin.native.cocoapods.archs="$ARCHS" \
+                    -Pkotlin.native.cocoapods.configuration="$CONFIGURATION"
+            SCRIPT
+        }
+    ]
+                
+end
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     kotlin("multiplatform") version "1.6.21"
+    kotlin("native.cocoapods") version "1.6.21"
     id("com.android.library")
     id("maven-publish")
 }
 
-val libraryVersion = "0.0.1"
+val libraryVersion = "0.0.3"
 var libraryGroup = "com.vivek"
 var libraryArtifactId = "kmm-module"
 
@@ -16,6 +17,28 @@ repositories {
 kotlin {
     android {
         publishLibraryVariants("release", "debug")
+    }
+    cocoapods {
+        // Required properties
+        // Specify the required Pod version here. Otherwise, the Gradle project version is used.
+        version = "0.0.1"
+        summary = "Some description for a Kotlin/Native module"
+        homepage = "Link to a Kotlin/Native module homepage"
+
+        // Optional properties
+        // Configure the Pod name here instead of changing the Gradle project name
+        name = "MyCocoaPod"
+
+        framework {
+            // Required properties
+            // Framework name configuration. Use this property instead of deprecated 'frameworkName'
+            baseName = "MyFramework"
+
+            // Optional properties
+            // Dynamic framework support
+            isStatic = false
+
+        }
     }
     iosX64()
     iosArm64()
@@ -64,6 +87,14 @@ publishing {
                     username = (System.getenv("GITHUB_USER") ?: project.properties["GITHUB_USER"]).toString()
                     password = (System.getenv("GITHUB_PERSONAL_ACCESS_TOKEN") ?: project.properties["GITHUB_PERSONAL_ACCESS_TOKEN"]).toString()
                 }
+            }
+        }
+        publications.withType<MavenPublication> {
+            println(artifactId)
+            artifactId = if (name == "kotlinmultiplatfromproject") {
+                artifactId.toLowerCase()
+            } else {
+                "$artifactId-$name".toLowerCase()
             }
         }
     }
